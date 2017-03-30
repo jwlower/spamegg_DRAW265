fa504ff5a84b556a22e81875485eef6bfea8a8cc
lloucks <liamloucks@hotmail.com>
2017-03-30 16:07:56 -0700

    Update triangle_mesh.sh

--- DragonDraw/triangle_mesh.sh
+++ DragonDraw/triangle_mesh.sh
@@ -7,7 +7,7 @@ fi
 
 
 # generate dragons and transform the colours and positions
-python generate_dragon.py $1 20 Pieces 2.0 > dragon.txt
+python generate_dragon.py $1 20 Growing_Pieces 2.0 > dragon.txt
 
 #convert to svg
 python lines_to_svg_colour.py dragon.txt > triangle_mesh.svg
